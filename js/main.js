085ef4a63e2156358c5939eef5531337767df9e0
Arpan B <arpanbhalla@gmail.com>
2016-11-10 09:35:23 +1100

    first commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var count = 0;
+
+$(function() {
+    var bingo = {
+        selectedNumbers: [],
+        generateRandom: function() {
+            var min = 1;
+            var max = 99;
+            var random = Math.floor(Math.random() * (max - min + 1)) + min;
+            return random;
+
+        },
+        boardReset: function(){
+            $("td").removeClass("selected");
+            bingo.selectedNumbers=[];
+            $(".bigNumberDisplay span").text("0");
+
+
+        },
+
+        houseCompleted: function(random){
+            bingo.selectedNumbers.push(random);
+            console.log("Length of array: " + bingo.selectedNumbers.length);
+            console.log(bingo.selectedNumbers);
+        }
+
+    };
+    $('td').each(function() {
+        var rowCount = $('td').length;
+        console.log("Length :"+ rowCount);
+        var concatClass = this.cellIndex+1;
+        var numberString = parseInt(concatClass, 10);
+        $(this).addClass("cell" + numberString).text(numberString);
+    });
+
+
+    $("#btnGenerate").on("click", function(){
+        if ((bingo.selectedNumbers.length)===5) {
+            bingo.boardReset();
+            count +=1;
+            alert ("House " + count + " Completed");
+            $(".gameCounter span").text(count);
+        } else {
+            var random = bingo.generateRandom();
+            bingo.houseCompleted(random);
+            console.log(random);
+            $(".bigNumberDisplay span").text(random);
+            $('td.cell+1' + random).addClass('selected');
+        }
+    });
+});
